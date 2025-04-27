5f8b03316f856bee6e70dabd7440c66fa0969a76
germdz <gerardo.montivero@mail.com>
2025-04-27 19:23:46 -0300

    Creando usuarios. Bug nelmio. Modificando readme. Agregando readme.

--- src/Command/CrearUsuarioPanelCommand.php
+++ src/Command/CrearUsuarioPanelCommand.php
@@ -8,6 +8,8 @@ use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Question\Question;
+use Symfony\Component\Console\Question\ChoiceQuestion;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
@@ -23,25 +25,59 @@ class CrearUsuarioPanelCommand extends Command
 
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
-        $role_user = new Role();
-        $role_user->setNombre('USER');
-        $role_user->setSlug('ROLE_USER');
-        $this->em->persist($role_user);
-        $role_admin = new Role();
-        $role_admin->setNombre('ADMIN');
-        $role_admin->setSlug('ROLE_ADMIN');
-        $this->em->persist($role_admin);
+        $helper = $this->getHelper('question');
+
+        // Preguntar email
+        $emailQuestion = new Question('📧 Ingrese el email del nuevo usuario: ');
+        $email = $helper->ask($input, $output, $emailQuestion);
+
+        // Preguntar contraseña (oculta)
+        $passwordQuestion = new Question('🔒 Ingrese la contraseña: ');
+        $passwordQuestion->setHidden(true);
+        $passwordQuestion->setHiddenFallback(false);
+        $password = $helper->ask($input, $output, $passwordQuestion);
+
+        // Confirmar contraseña
+        $passwordConfirmQuestion = new Question('🔒 Confirme la contraseña: ');
+        $passwordConfirmQuestion->setHidden(true);
+        $passwordConfirmQuestion->setHiddenFallback(false);
+        $passwordConfirm = $helper->ask($input, $output, $passwordConfirmQuestion);
+
+        if ($password !== $passwordConfirm) {
+            $output->writeln('<error>❌ Las contraseñas no coinciden.</error>');
+            return Command::FAILURE;
+        }
+
+        // Crear roles si no existen
+        $roleUser = $this->em->getRepository(Role::class)->findOneBy(['slug' => 'ROLE_USER']);
+        if (!$roleUser) {
+            $roleUser = new Role();
+            $roleUser->setNombre('Usuario');
+            $roleUser->setSlug('ROLE_USER');
+            $this->em->persist($roleUser);
+        }
+
+        $roleAdmin = $this->em->getRepository(Role::class)->findOneBy(['slug' => 'ROLE_ADMIN']);
+        if (!$roleAdmin) {
+            $roleAdmin = new Role();
+            $roleAdmin->setNombre('Administrador');
+            $roleAdmin->setSlug('ROLE_ADMIN');
+            $this->em->persist($roleAdmin);
+        }
+
         $this->em->flush();
+
+        // Crear el usuario
         $usuario = new UsuarioPanel();
-        $usuario->setEmail('admin@sitio.ar');
-        $usuario->addRole($role_user);
-        $usuario->addRole($role_admin);
-        $usuario->setPassword($this->hasher->hashPassword($usuario, '123456'));
+        $usuario->setEmail($email);
+        $usuario->addRole($roleUser);
+        $usuario->addRole($roleAdmin);
+        $usuario->setPassword($this->hasher->hashPassword($usuario, $password));
 
         $this->em->persist($usuario);
         $this->em->flush();
 
-        $output->writeln('UsuarioPanel creado con éxito!');
+        $output->writeln('<info>✅ UsuarioPanel creado con éxito!</info>');
         return Command::SUCCESS;
     }
 }
