8f90ce678104c9bbcb3e12f20bb9df10f89f690f
germdz <gerardo.montivero@mail.com>
2025-04-30 19:47:22 -0300

    Agregando controller para members. Agregando members. Agregando show. Agregando auditId en la creación de usuarios panel.

--- src/Controller/MemberController.php
+++ src/Controller/MemberController.php
@@ -4,27 +4,24 @@ namespace App\Controller;
 
 use App\Entity\Member;
 use App\Repository\MemberRepository;
+use App\Repository\UsuarioPanelRepository;
 use DateTime;
+use DateTimeImmutable;
 use Doctrine\ORM\EntityManagerInterface;
 use Exception;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\JsonResponse;
-use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Attribute\Route;
 
 #[Route('/api/members', name: 'api_members_')]
 class MemberController extends AbstractController
 {
-    #[Route('', name: 'index', methods: ['GET'])]
-    public function index(MemberRepository $repo): JsonResponse
+    #[Route('', name: 'list', methods: ['GET'])]
+    public function list(MemberRepository $memberRepository): JsonResponse
     {
-        return $this->json($repo->findAll());
-    }
-
-    #[Route('/{id}', name: 'show', methods: ['GET'])]
-    public function show(Member $member): JsonResponse
-    {
-        return $this->json($member);
+        $members = $memberRepository->findBy([], ['lastname' => 'ASC']);
+        return $this->json($members);
     }
 
     /**
@@ -36,39 +33,140 @@ class MemberController extends AbstractController
         $data = json_decode($request->getContent(), true);
 
         $member = new Member();
-        $member->setName($data['name'] ?? '');
-        $member->setLastname($data['lastname'] ?? '');
+        $member->setName($data['name'] ?? null);
+        $member->setLastname($data['lastname'] ?? null);
         $member->setBirthdate(new DateTime($data['birthdate']));
         $member->setDniDocument($data['dni_document'] ?? '');
         $member->setAddress($data['address'] ?? '');
-        $member->setGenderId($data['gender_id']);
-        $member->setCivilStateId($data['civil_state_id']);
-        // Y así con el resto...
+        $member->setEmail($data['email'] ?? null);
+        $member->setPhone($data['phone'] ?? null);
+        $member->setGender($data['gender_id'] ?? 0);
+        $member->setCivilStateId($data['civil_state_id'] ?? 0);
+        $member->setPathPhoto($data['path_photo'] ?? null);
+        $member->setNameProfession($data['name_profession'] ?? null);
+        $member->setArtisticSkills($data['artistic_skills'] ?? null);
+        $member->setCountryId($data['country_id'] ?? null);
+        $member->setStateId($data['state_id'] ?? null);
+        $member->setDistrictId($data['district_id'] ?? null);
+        $member->setLocalitiesId($data['localities_id'] ?? null);
+        $member->setBossFamily($data['boss_family'] ?? false);
+        $member->setQuantitySons($data['quantity_sons'] ?? null);
+        $member->setCelebracion($data['celebracion'] ?? null);
+        $member->setNameGuia($data['name_guia'] ?? null);
+        $member->setNameGroup($data['name_group'] ?? null);
+        $member->setGrupo($data['grupo'] ?? null);
+        $member->setParticipateGp($data['participate_gp'] ?? null);
+
+        $member->setAudiUser($this->getUser()?->getAuditId() ?? null);
+        $member->setAudiDate(new DateTime());
+        $member->setAudiAction('I');
 
         $em->persist($member);
         $em->flush();
 
-        return $this->json(['id' => $member->getId()], 201);
+        return $this->json($member);
     }
 
-    #[Route('/{id}', name: 'update', methods: ['PUT'])]
-    public function update(Member $member, Request $request, EntityManagerInterface $em): JsonResponse
+    /**
+     * @throws Exception
+     */
+    #[Route('/{id}', name: 'update', methods: ['PUT', 'PATCH'])]
+    public function update(Request $request, Member $member, EntityManagerInterface $em): JsonResponse
     {
         $data = json_decode($request->getContent(), true);
+
         $member->setName($data['name'] ?? $member->getName());
-        // Y lo demás igual...
+        $member->setLastname($data['lastname'] ?? $member->getLastname());
+        $member->setBirthdate(isset($data['birthdate']) ? new DateTime($data['birthdate']) : $member->getBirthdate());
+        $member->setDniDocument($data['dni_document'] ?? $member->getDniDocument());
+        $member->setAddress($data['address'] ?? $member->getAddress());
+        $member->setEmail($data['email'] ?? $member->getEmail());
+        $member->setPhone($data['phone'] ?? $member->getPhone());
+        $member->setGender($data['gender_id'] ?? $member->getGender());
+        $member->setCivilStateId($data['civil_state_id'] ?? $member->getCivilStateId());
+        $member->setPathPhoto($data['path_photo'] ?? $member->getPathPhoto());
+        $member->setNameProfession($data['name_profession'] ?? $member->getNameProfession());
+        $member->setArtisticSkills($data['artistic_skills'] ?? $member->getArtisticSkills());
+        $member->setCountryId($data['country_id'] ?? $member->getCountryId());
+        $member->setStateId($data['state_id'] ?? $member->getStateId());
+        $member->setDistrictId($data['district_id'] ?? $member->getDistrictId());
+        $member->setLocalitiesId($data['localities_id'] ?? $member->getLocalitiesId());
+        $member->setBossFamily($data['boss_family'] ?? $member->isBossFamily());
+        $member->setQuantitySons($data['quantity_sons'] ?? $member->getQuantitySons());
+        $member->setCelebracion($data['celebracion'] ?? $member->getCelebracion());
+        $member->setNameGuia($data['name_guia'] ?? $member->getNameGuia());
+        $member->setNameGroup($data['name_group'] ?? $member->getNameGroup());
+        $member->setGrupo($data['grupo'] ?? $member->getGrupo());
+        $member->setParticipateGp($data['participate_gp'] ?? $member->getParticipateGp());
+
+        $member->setAudiUser($this->getUser()?->getAuditId() ?? null);
+        $member->setAudiDate(new DateTime());
+        $member->setAudiAction('U');
 
         $em->flush();
 
-        return $this->json(['message' => 'Actualizado']);
+        return $this->json($member);
     }
 
-    #[Route('/{id}', name: 'delete', methods: ['DELETE'])]
-    public function delete(Member $member, EntityManagerInterface $em): JsonResponse
+    /**
+     * @throws Exception
+     */
+    #[Route('/{id}', methods: ['DELETE'])]
+    public function delete(int $id, Request $request, EntityManagerInterface $em, MemberRepository $memberRepository): JsonResponse
     {
-        $em->remove($member);
+        $member = $memberRepository->find($id);
+
+        if (!$member) {
+            return new JsonResponse(['error' => 'Miembro no encontrado.'], 404);
+        }
+
+        $data = json_decode($request->getContent(), true);
+
+        $member->setAudiUser($this->getUser()?->getAuditId() ?? null);
+        $member->setAudiDate(new DateTimeImmutable($data['audi_date'] ?? 'now'));
+        $member->setAudiAction('D');
+
         $em->flush();
 
-        return $this->json(['message' => 'Eliminado']);
+        return new JsonResponse(['message' => 'Miembro marcado como eliminado.']);
+    }
+
+    #[Route('/{id}', name: 'show', methods: ['GET'])]
+    public function show(Member $member, UsuarioPanelRepository $usuarioPanelRepository): JsonResponse
+    {
+        static $usuariosCache = [];
+
+        $auditUser = null;
+        $idAudit = $member->getAudiUser();
+
+        if ($idAudit) {
+            if (!isset($usuariosCache[$idAudit])) {
+                $userAction = $usuarioPanelRepository->findOneBy(['auditId' => $idAudit]);
+                $usuariosCache[$idAudit] = $userAction;
+            } else {
+                $userAction = $usuariosCache[$idAudit];
+            }
+
+            if ($userAction) {
+                $auditUser = [
+                    'nombre' => $userAction->getNombre(),
+                    'email' => $userAction->getEmail()
+                ];
+            }
+        }
+
+        return $this->json([
+            'id' => $member->getId(),
+            'name' => $member->getName(),
+            'lastname' => $member->getLastname(),
+            'birthdate' => $member->getBirthdate()?->format('Y-m-d'),
+            'dni_document' => $member->getDniDocument(),
+            'address' => $member->getAddress(),
+            'email' => $member->getEmail(),
+            'phone' => $member->getPhone(),
+            'audi_user' => $auditUser,
+            'audi_date' => $member->getAudiDate()?->format('Y-m-d H:i:s'),
+            'audi_action' => $member->getAudiAction(),
+        ]);
     }
 }
