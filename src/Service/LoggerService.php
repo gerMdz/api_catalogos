903edf35ccb191863b2f3654a6c4758215ea9866
germdz <gerardo.montivero@mail.com>
2025-05-20 22:22:34 -0300

    Ajustando familia para los errores Agregando excepciones. Bug json need.

--- src/Service/LoggerService.php
+++ src/Service/LoggerService.php
@@ -8,7 +8,6 @@ use Doctrine\ORM\EntityManagerInterface;
 class LoggerService
 {
 
-    private const VALID_TYPES = ['error', 'warning', 'data_corruption'];
 
     public function __construct(private readonly EntityManagerInterface $em)
     {
@@ -16,9 +15,7 @@ class LoggerService
 
     public function log(string $type, string $message, ?array $context = null): void
     {
-        if (!in_array($type, self::VALID_TYPES, true)) {
-            throw new \InvalidArgumentException("Tipo de log inválido: '$type'. Valores permitidos: " . implode(', ', self::VALID_TYPES));
-        }
+
 
         $log = new ApiLog($type, $message, $context);
         $this->em->persist($log);
